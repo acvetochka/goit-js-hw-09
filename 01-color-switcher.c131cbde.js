4264eda6e68597b2abc0829095a88deb4e354301
acvetochka <116402791+acvetochka@users.noreply.github.com>
2023-03-24 16:35:55 +0000

    Deploying to gh-pages from @ acvetochka/goit-js-hw-09@6a0522a040968c2c79c3f4c08e30f88318c3c9a2 🚀

---
+++ 01-color-switcher.c131cbde.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled","disabled"),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.c131cbde.js.map
