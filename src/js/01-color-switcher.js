6a0522a040968c2c79c3f4c08e30f88318c3c9a2
Alona Kuznietsova <acvetochka@gmail.com>
2023-03-24 17:35:20 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let interval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const onColor = () => {
+  interval = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+};
+
+const offColor = () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(interval);
+};
+
+startBtn.addEventListener('click', onColor);
+stopBtn.addEventListener('click', offColor);
